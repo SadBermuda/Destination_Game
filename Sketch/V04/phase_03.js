b88f967b9ff33318e06eee846a6e5d6851bb6585
Mathias Hangartner <mathias.hangartner@gmail.com>
2018-01-17 22:44:14 +0100

    Finalisation pour presentation 18.01.18
    BUG - disparition des sprites de la première phase

--- Sketch/V04/phase_03.js
+++ Sketch/V04/phase_03.js
@@ -1,10 +1,26 @@
 var bkg;
-
-function preload () {
-  bkg = loadImage ("assets/Scene_03/HEAD0027_Creative_Coding_Canvas_Animation_00_bkg.png");
-}
+var rocket;
+var GRAVITY = .5;
 
 function setup () {
   createCanvas (800, 800);
+  bkg = loadImage ("assets/Scene_03/HEAD0027_Creative_Coding_Canvas_Animation_00_bkg.png");
+  rocket = loadImage ("assets/Scene_03/HEAD0027_Creative_Coding_Canvas_Animation_01_rocket.png");
+  rocketSprite = createSprite(400,-200);
+  rocketSprite.addImage(rocket);
+}
+
+function draw () {
   image(bkg,0,0);
+
+  rocketSprite.velocity.y = 2;
+
+  if (keyIsPressed) {
+  rocketSprite.velocity.y= GRAVITY;
+  }
+
+  if (rocketSprite.position.y > 400) {
+  rocketSprite.velocity.y= 0;
+  }
+  drawSprites();
 }
--- Sketch/V04/sketch.js
+++ Sketch/V04/sketch.js
@@ -1,29 +1,39 @@
 var bkg;
 var rocket;
 var platform;
-var GRAVITY = 1;
 
 function setup() {
   createCanvas(800, 800);
   bkg = loadImage("assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_00_BKG.png");
 
   rocket = createSprite(400, 390);
-  rocket.addAnimation("start", "assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_01_fusee_demarrage_01.png", "assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_01_fusee_demarrage_05.png");
-  rocket.addAnimation("flight", "assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_02_fusee_launch_01.png", "assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_02_fusee_launch_03.png");
+  rocket.addAnimation("start", "assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_01_fusee_demarrage_0001.png", "assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_01_fusee_demarrage_0005.png");
+  rocket.addAnimation("flight", "assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_02_fusee_launch_0001.png", "assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_02_fusee_launch_0003.png");
   rocket.setCollider("rectangle", 0, 0, 400, 390);
 
   var img_platform = loadImage("assets/Scene_01/HEAD0027_Creative_Coding_Canvas_Animation_Collider.png");
-  platform = createSprite(400, 390, 400, 10);
+  platform = createSprite(400, 390,);
+  platform.addImage(img_platform);
 
 
 }
 
 function draw() {
+  image(bkg, 0,0);
 
-  rocket.velocity.y += GRAVITY;
   if(rocket.collide(platform)) {
     rocket.velocity.y = 0;
     rocket.changeAnimation("start");
+  }
+
+  if(keyIsPressed) {
+    rocket.changeAnimation("flight");
+    rocket.velocity.y = 2;
+  }
+
+  if (rocket.position.y > 1000) {
+    delay = 800;
+    window.location.href = "phase_02.html";
   }
 
   drawSprites();
